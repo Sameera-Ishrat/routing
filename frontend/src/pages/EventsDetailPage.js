071e74e5c8f311c32b4b408d60954072622c4cc5
Sameera Ishrat <ss1304@yahoo.com>
2023-08-11 19:31:02 -0700

    Added action for submitting the form

--- frontend/src/pages/EventsDetailPage.js
+++ frontend/src/pages/EventsDetailPage.js
@@ -1,16 +1,31 @@
 import React from 'react'
-import { useParams } from 'react-router'
-import { Link } from 'react-router-dom';
+// import { useParams } from 'react-router'
+// import { Link } from 'react-router-dom';
+
+import EventItem from "../components/EventItem";
+import { json, useLoaderData, useRouteLoaderData } from 'react-router';
+
+
 const EventsDetailPage = () => {
-   const params =  useParams();
+  
+//   const data = useLoaderData();
+// console.log(data.event);
+
+const data = useRouteLoaderData('event-details')
   return (
-    <>
-     <h2>Events details page</h2> 
-    <p>{params.eventId}</p>
-    <Link to= ".." relative='path'>Back</Link>
-    </>
-   
+  <EventItem  event = {data.event}/>
   )
+  // event is coming from yhr backend
 }
 
 export default EventsDetailPage
+
+export const loader = async({request,params}) => {
+const id = params.eventId;
+const response = await fetch(`http://localhost:8080/events/${id}`);
+if(!response.ok) {
+  throw json({message:'Cannot load Event Details'},{status:500})
+}else {
+  return response;
+}
+}
--- frontend/src/pages/EventsNavigation.js
+++ frontend/src/pages/EventsNavigation.js
@@ -1,19 +1,25 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const EventsNavigation = () => {
   return (
     <nav>
-        <ul className='events-list'>
-            <NavLink to='new-event'
-            className={({isActive}) => isActive ? "active" : "undefined"}
-            >New Event</NavLink>
-            <NavLink to='/events/edit'
-            className={({isActive}) => isActive ? "active" : "undefined"}
-            >Edit Event</NavLink>
-        </ul>
+      <ul className="events-list">
+        <NavLink
+          to=""
+          className={({ isActive }) => (isActive ? "active" : "undefined")}
+        >
+          All Event
+        </NavLink>
+        <NavLink
+          to="new-event"
+          className={({ isActive }) => (isActive ? "active" : "undefined")}
+        >
+          New Event
+        </NavLink>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default EventsNavigation
+export default EventsNavigation;
